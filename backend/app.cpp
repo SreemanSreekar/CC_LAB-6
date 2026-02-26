f930ef7c7d5cb23763ae82041e5c091e2005398f
Sreeman Sreekar <pes1ug23cs309@pesu.pes.edu>
2026-02-26 06:50:36 +0000

    Added Dockerfile and backend code

---
+++ backend/app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unistd.h>
+
+int main() {
+    while(true) {
+        std::cout << "Served by backend container" << std::endl;
+        sleep(5);
+    }
+    return 0;
+}
